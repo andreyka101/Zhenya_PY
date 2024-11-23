70fd6276d4e2b04e6bb978fa0b6d1a08c0c2848e
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-23 18:13:31 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,74 @@
+# print("hello world")
+# print('text')
+# print(12)
+
+
+
+# num = 2
+# print(num)
+
+
+
+# red_car = 9
+# redCar = 9
+# redCarSuper = 9
+
+
+
+# num_2 = 6
+# print(2 + 2)
+# print(2 + num_2)
+
+
+
+# print("hello" , 4 , num_2)
+
+
+
+
+# print("5 + 2 =" , 5 + 2)
+# print("5 - 2 =" , 5 - 2)
+# print("3 * 2 =" , 3 * 2)
+# print("3 ** 2 =" , 3 ** 2)
+# print("10 / 3 =" , 10 / 3)
+# print("10 // 3 =" , 10 // 3)
+# print("10 % 3 =" , 10 % 3)
+
+
+
+# num_3 = 5 + 2 
+# print(num_3)
+
+# num_3 = 2
+# print(num_3)
+
+# num_3 = num_3 + 3
+# print(num_3)
+
+
+
+# num_int = 4
+# num_float = 5.7
+# num_str = "w1e2r"
+# num_bool = False
+# num_none = None
+
+
+# print("int =" , num_int)
+# print("float =" , num_float)
+# print("str =" , num_str)
+# print("bool =" , num_bool)
+# print("none =" , num_none)
+
+
+
+# num_str = "svg" + "90 " +"w"
+# print(num_str)
+
+
+
+# print("svg-" * 4)
+
+
+
+
