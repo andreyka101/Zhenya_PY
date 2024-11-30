3b435e2ca5d19bdf0df68af293914ad8db8d0ef2
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-30 18:21:35 +0300

    efefef

---
+++ lesson_2_string.py
@@ -0,0 +1,105 @@
+
+# print(type("3.0"))
+
+
+# num = 2
+# print(type(num))
+
+
+
+# num_type = type(num)
+# print(type(num_type))
+
+
+
+# num_2 = 4
+# num_str_2 = str(num_2)
+# print("num_2 =" ,num_2)
+# print(type(num_2))
+# print("-----------")
+# print("num_str_2 =" ,num_str_2)
+# print(type(num_str_2))
+
+
+
+# num_3 = 6
+# num_str_3 = float(num_3)
+# print("num_3 =" ,num_3)
+# print(type(num_3))
+# print("-----------")
+# print("num_str_3 =" ,num_str_3)
+# print(type(num_str_3))
+
+
+
+# num_4 = 6.7
+# num_str_4 = int(num_4)
+# print("num_4 =" ,num_4)
+# print(type(num_4))
+# print("-----------")
+# print("num_str_4 =" ,num_str_4)
+# print(type(num_str_4))
+
+
+
+# num_5 = "8"
+# num_int_5 = int(num_5)
+# print("num_5 =" ,num_5)
+# print(type(num_5))
+# print("-----------")
+# print("num_int_5 =" ,num_int_5)
+# print(type(num_int_5))
+
+
+
+# num_6 = -10
+# num_bool_6 = bool(num_6)
+# print("num_6 =" ,num_6)
+# print(type(num_6))
+# print("-----------")
+# print("num_bool_6 =" ,num_bool_6)
+# print(type(num_bool_6))
+
+
+
+# num_7 = False
+# num_int_7 = int(num_7)
+# print("num_7 =" ,num_7)
+# print(type(num_7))
+# print("-----------")
+# print("num_int_7 =" ,num_int_7)
+# print(type(num_int_7))
+
+
+
+# num_inp_1 = input("==>")
+# print("num_inp_1 =" , num_inp_1)
+# print(type(num_inp_1))
+
+
+
+# num_inp_2 = input("==>")
+# anwser_2 = int(num_inp_2)
+# print("ответ: " , anwser_2 * 12)
+
+
+
+# num_inp_3 = int(input("==>"))
+# print("ответ: " , num_inp_3 * 12)
+
+
+
+# str_1 = "q🐪werty"
+# print(str_1)
+# print(str_1[1] , str_1[5])
+# sumbol =  str_1[1] + str_1[5] + "*123"
+# print(sumbol)
+
+
+
+# print("1qewe ' ee")
+# print("1qewe \" ee")
+# print("1qewe \n ee")
+# print("1qewe \t ee")
+
+
