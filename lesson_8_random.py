5976eb184e083669397a09e5dbd635a7294f6a09
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-29 18:40:06 +0300

    wdwdwd

--- lesson_8_random.py
+++ lesson_8_random.py
@@ -1,8 +1,13 @@
+
+# подключаем библиотеку random
 import random
 
+# .random() - генерирует случайное число
 # print(random.random())
 
+# .randint(min, max) - генерирует случайное число от min до max
 # print(random.randint(1,10))
 
+# получаем случайный элемент списка
 # arr = [1,2,3,4,5]
 # print(arr[random.randint(0,4)])
