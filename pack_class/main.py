97d61334e10700c46ddcc2a4a26212d5ca51592f
andreyka101 <andrejkvasnin236@gmail.com>
2025-02-03 19:39:34 +0300

    цвцвцвцв

--- pack_class/main.py
+++ pack_class/main.py
@@ -1,3 +1,5 @@
+
+# импортируем переменные из другого файла py
 # from number_fun import num , arr , fun
 
 # print(num)
@@ -6,6 +8,7 @@
 
 
 
+# импортируем все переменные из другого файла py
 # from number_fun import *
 
 # print(arr)
@@ -13,6 +16,7 @@
 
 
 
+# импортируем файл py
 # import number_fun
 
 # number_fun.fun()
@@ -20,6 +24,7 @@
 
 
 
+# импортируем файл py и сохраняем под другим именем
 # import number_fun as file_x
 
 # file_x.fun()
@@ -28,6 +33,7 @@
 
 
 
+# импортируем класс из другого файла py
 from lesson_14_class import All_animals
 dog_1 = All_animals("werty")
 dog_1.dogs.fun()
