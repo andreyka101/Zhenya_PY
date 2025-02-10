c2041a5ee62d177dccb9c02aa794a391c8a60aab
andreyka101 <andrejkvasnin236@gmail.com>
2025-02-10 17:13:27 +0300

    цвцввц

--- lesson_16_time.py
+++ lesson_16_time.py
@@ -1,9 +1,16 @@
+
+# подключаем библитеку time
 # import time 
 
+
+# .time() - получить unix время
 # print(time.time())
+# формула рандомного числа
 # print(int(time.time() * 1000000) % (20 - 1 + 1) + 1)
 
 
+
+# .sleep(s) - программа останавливается на s секунд
 # print(1)
 # time.sleep(3)
 # print(2)
@@ -11,17 +18,24 @@
 
 
 
+
+
+# подключаем библитеку datetime
 import datetime
 
 time_now = datetime.datetime.now()
 
+
+# получаем дату и время
 print(time_now)
 
 
+# получаем дату
 # print(time_now.date())
 # print(time_now.date().day)
 
 
+# получаем время
 # print(time_now.time())
 # print(time_now.time().minute)
 # print(time_now.time().second)
--- lesson_17_json.py
+++ lesson_17_json.py
@@ -1,5 +1,8 @@
 import json
 
+
+
+# json.dumps - переводит в json строку
 # arr = [1,2,3,4,5,]
 # print(arr[0])
 # str_arr_j = json.dumps(arr)
@@ -8,6 +11,7 @@ import json
 
 
 
+# перезапись файла json
 # file = open("j_file_1.json" , "w")
 # file.write(str_arr_j)
 # file.read()
@@ -15,6 +19,7 @@ import json
 
 
 
+# json.load - переводит открытый файл в list или dict
 # file = open("j_file_1.json" , "r")
 # arr_2 = json.loads(file.read())
 # print(arr_2)
@@ -22,6 +27,7 @@ import json
 
 
 
+# json.load - записывает list или dict в открытый файл
 s = "key"
 obj = {
     "yfwfd":266,
@@ -29,12 +35,12 @@ obj = {
     3:"qq",
 }
 # print(obj["key"])
-
 file_3 = open("j_file_1.json" , "w")
 json.dump(obj , file_3)
 
 
 
+# json.load - переводит данные открытого файла в list или dict
 file_3 = open("j_file_1.json" , "r")
 arr_3 = json.load(file_3)
 print(arr_3)
